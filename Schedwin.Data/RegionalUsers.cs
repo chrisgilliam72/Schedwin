0e643ba080b5358f06c5e42c16c9704f568eaf9b
chrisgilliam72 <54978118+chrisgilliam72@users.noreply.github.com>
2020-08-26 13:38:26 +0200

    Add files via upload

---
+++ Schedwin.Data/RegionalUsers.cs
@@ -0,0 +1,74 @@
+﻿using Schedwin.Common;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedwin.Data
+{
+    public class RegionalUsers
+    {
+        public String LastError { get; set; }
+        public async Task<bool> CreateUser(String firstName, String surname, String adUsername, int companyID, String email, String Server, String regionalDatabase)
+        {
+            try
+            {
+
+                var constring = RegionalConnectionGenerator.GetConnectionString(Server, regionalDatabase);
+                var ctx = new SchedwinRegionalEntities(constring);
+                var schedUser = await ctx.tset_Personnel.FirstOrDefaultAsync(x => x.Username == adUsername);
+                if (schedUser==null)
+                {
+                    var newuser = new tset_Personnel();
+                    newuser.IDX_Company = companyID;
+                    newuser.Firstname = firstName;
+                    newuser.Surname = surname;
+                    newuser.Telephone = "1";
+                    newuser.Fax = "1";
+                    newuser.DateOfBirth = DateTime.Today;
+                    newuser.EMail = email;
+                    newuser.Username = adUsername;
+                    newuser.WebPassword = "";
+                    newuser.Email2 = email;
+                    newuser.IDX_PersonnelType = 1;
+                    ctx.tset_Personnel.Add(newuser);
+                    await ctx.SaveChangesAsync();
+                }
+
+                return true;
+            }
+            catch (Exception ex )
+            {
+                var messages = ex.FromHierarchy(x => x.InnerException).Select(x => x.Message);
+                LastError = string.Join(Environment.NewLine, messages);
+                return false;
+
+            }
+        }
+
+       public  async Task<int> GetRegionalIDX(String adUsername,String Server,String regionalDatabase)
+       {
+            try
+            {
+                var constring = RegionalConnectionGenerator.GetConnectionString(Server, regionalDatabase);
+                var ctx = new SchedwinRegionalEntities(constring);
+
+                using (ctx)
+                {
+                    var schedUser = await ctx.tset_Personnel.FirstOrDefaultAsync(x => x.Username == adUsername && x.Active.HasValue && x.Active.Value);
+                    return schedUser != null ? schedUser.IDX : -1;
+                }
+            }
+            catch (Exception ex)
+            {
+                var messages = ex.FromHierarchy(x => x.InnerException).Select(x => x.Message);
+                LastError = string.Join(Environment.NewLine, messages);
+                return -999;
+            }
+
+               
+        }
+    }
+}
---
+++ Schedwin.Data/SchedwinRegionalContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedwin.Data
+{
+   public class RegionalConnectionGenerator
+    {
+        public static String GetConnectionString(String Server, String datbaseName)
+        {
+            SqlConnectionStringBuilder sqlString = new SqlConnectionStringBuilder()
+            {
+                IntegratedSecurity = true,
+                DataSource = Server,
+                InitialCatalog = datbaseName,
+                ConnectTimeout=60,
+                MultipleActiveResultSets = true
+            };
+
+
+            EntityConnectionStringBuilder entityString = new EntityConnectionStringBuilder()
+            {
+                Provider = "System.Data.SqlClient",
+                Metadata = "res://*/SchedwinRegional.csdl|res://*/SchedwinRegional.ssdl|res://*/SchedwinRegional.msl",
+                ProviderConnectionString = sqlString.ToString()
+            };
+
+            return entityString.ConnectionString;
+        }
+
+
+    }
+   partial class  SchedwinRegionalEntities
+    {
+        public SchedwinRegionalEntities(String connectionString)
+            : base(connectionString)
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            var constring = new EntityConnectionStringBuilder();
+        }
+
+
+    }
+}
