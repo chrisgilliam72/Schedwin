e05318046c12692d2448b6ada3c23fbdd1e64e9b
chrisgilliam72 <54978118+chrisgilliam72@users.noreply.github.com>
2020-08-26 13:29:55 +0200

    Add files via upload

---
+++ Schedwin.Data.Wish/WishAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedwin.Data.Wish
+{
+    public class WishAccess
+    {
+        public async Task<List<String>> GetWishConsultants()
+        {
+            var ctx = new WISHEntities();
+            using (ctx)
+            {
+               var users= await ctx.tbUsers.ToListAsync();
+                return users.Select(x => x.PersonCode).ToList();
+
+            }
+        }
+    }
+}
